5f8cf4308789c9cd64e45240cefd99f6a0a95cdc
Ahnue999 <ahnue200572@gmail.con>
2024-05-15 18:11:32 +0300

    commit

---
+++ models/base_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """ AirBnb's base model that defines all attributes
+        and methods. """
+
+    def __init__(self, *args, **kwargs):
+        """ initializiation function """
+        if kwargs == {}:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            kwargs.pop("__class__")
+            self.__dict__.update(**kwargs)
+
+            # Converting strings data to datatime format.
+            cr, up = kwargs["created_at"], kwargs["updated_at"]
+            cr = datetime.strptime(cr, format("%Y-%m-%dT%H:%M:%S.%f"))
+            up = datetime.strptime(up, format("%Y-%m-%dT%H:%M:%S.%f"))
+            self.__dict__.update({"created_at": cr, "updated_at": up})
+
+    def __str__(self):
+        """ prints data about an instance:
+            format:
+            [<class name>] (<self.id>) <self.__dict__>
+            """
+        form = f"[BaseModel] ({self.id}) {self.__dict__}"
+        print(form)
+
+        return (form)
+
+    def save(self):
+        """ updates the @update_at public intance attribute
+            with the current date time. """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values
+            of __dict__ of the instance. """
+
+        instance_dict = dict()
+        instance_dict.update(**self.__dict__)
+        instance_dict.update({"__class__": "BaseModel",
+                              "created_at": self.created_at.isoformat(),
+                              "updated_at": self.created_at.isoformat()})
+
+        return (instance_dict)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import json
+
+
+class FileStorage:
+    """ Serializes instances to a JSON file and deserializes
+        JSON file to instances. """
+
+    __file_path = "file.json"
+    __objects = dict()
+
+    def all(self):
+        """ returns the dictionay @__objects. """
+
+        return (self.__objects)
+
+    def new(self, obj):
+        """ sets in @__objects the @obj with key:
+            <obj class name>.id """
+
+        name = str(obj.__class__) + "." + obj.id
+        self.__objects.update({name: obj})
+
+    def save(self):
+        """ serializes @__objects to the JSON file """
+
+        print("\n\n\n\n")
+        print(self.__file_path)
+        print("\n\n\n\n")
+        with open(self.__file_path, "w+", encoding="utf-8") as fp:
+            json.dump(self.__objects, fp)
+
+    def reload(self):
+        """ deserializes the JSON file to @__objects """
+
+        try:
+            with open(self.__file_path, "r", encoding="utf-8") as fp:
+                self.__objects = json.load(fp)
+        except (IOError, json.JSONDecodeError):
+            return
